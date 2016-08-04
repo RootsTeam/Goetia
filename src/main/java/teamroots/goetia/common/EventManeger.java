1a90e318a2374108ce8442c12dfc21aa3cafebd2
SirShadow <shadowgamerxy@gmail.com>
2016-08-04 22:59:30 +0200

    new item stuff

--- src/main/java/teamroots/goetia/common/EventManeger.java
+++ src/main/java/teamroots/goetia/common/EventManeger.java
@@ -1,28 +1,9 @@
 package teamroots.goetia.common;
 
-import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
-import net.minecraftforge.event.entity.living.LivingHurtEvent;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import teamroots.goetia.registry.MainRegistry;
-
 /**
  * Created by TeamRoots on 4.8.2016.
  */
 public class EventManeger
 {
-    @SubscribeEvent
-    public void onEntityHurt(LivingHurtEvent event) {
-        if (!(event.getEntity() instanceof EntityPlayer)) {
-            return;
-        }
 
-        EntityPlayer player = (EntityPlayer) event.getEntity();
-        if (player.inventory.getCurrentItem().getItem() == new ItemStack(MainRegistry.demonHornSpear).getItem()) {
-            player.getFoodStats().addStats(1, 0.5f);
-            // player.addChatComponentMessage(new TextComponentString("It works!"));
-        } else {
-            //player.addChatComponentMessage(new TextComponentString("Debug: doesnt work"));
-        }
-    }
 }
--- src/main/java/teamroots/goetia/common/items/base/ItemDemonicSpear.java
+++ src/main/java/teamroots/goetia/common/items/base/ItemDemonicSpear.java
@@ -5,11 +5,10 @@ import net.minecraftforge.common.util.EnumHelper;
 /**
  * Created by TeamRoots on 4.8.2016.
  */
-public class ItemDemonicSpear extends ItemSwordBase
-{
-    public static ToolMaterial demonic = EnumHelper.addToolMaterial("demonic",3,749,3,3.5f,3);
-    public ItemDemonicSpear()
-    {
-        super("demonHornSpear",demonic);
+public class ItemDemonicSpear extends ItemSwordBase {
+    public static ToolMaterial demonic = EnumHelper.addToolMaterial("demonic", 3, 749, 3, 3.5f, 3);
+
+    public ItemDemonicSpear() {
+        super("demonHornSpear", demonic);
     }
 }
