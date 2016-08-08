58021efc972bcecc16b6add1a9a93a834987be7c
elucent <elucentgames@gmail.com>
2016-08-08 03:35:53 -0400

    Little tweak.

--- src/main/java/teamroots/goetia/spellcasting/SpellInnerFire.java
+++ src/main/java/teamroots/goetia/spellcasting/SpellInnerFire.java
@@ -9,7 +9,7 @@ import teamroots.goetia.lib.LibMain;
 public class SpellInnerFire extends CastSpell{
 
 	public SpellInnerFire() {
-		super("innerFire", 111, new int[]{2,1,4,5,6}, new ItemStack(Items.FLINT_AND_STEEL));
+		super("innerFire", 111, new int[]{3,2,1,4,5,6}, new ItemStack(Items.FLINT_AND_STEEL));
 	}
 	
 	@Override
@@ -17,13 +17,13 @@ public class SpellInnerFire extends CastSpell{
 		for (float i = 0; i < 360; i += 7.5){
 			float offX = 0.75f*(float)Math.sin(Math.toRadians(i));
 			float offZ = 0.75f*(float)Math.cos(Math.toRadians(i));
-			if (random.nextInt(2) == 0){
+			if (random.nextInt(3) == 0){
 				caster.getEntityWorld().spawnParticle(EnumParticleTypes.FLAME, caster.posX+offX, caster.posY+caster.getEyeHeight()/2.0, caster.posZ+offZ, 0, 0, 0, 0);
 			}
-			if (random.nextInt(2) == 0){
-				caster.getEntityWorld().spawnParticle(EnumParticleTypes.CLOUD, caster.posX+offX, caster.posY+caster.getEyeHeight()/2.0, caster.posZ+offZ, 0, 0.5*random.nextFloat(), 0, 0);
+			if (random.nextInt(3) == 0){
+				caster.getEntityWorld().spawnParticle(EnumParticleTypes.SMOKE_LARGE, caster.posX+offX, caster.posY+caster.getEyeHeight()/2.0, caster.posZ+offZ, 0, 0.015*random.nextFloat(), 0, 0);
 			}
-			caster.getEntityWorld().spawnParticle(EnumParticleTypes.CLOUD, caster.posX+offX, caster.posY+caster.getEyeHeight()/2.0, caster.posZ+offZ, 0, 0.5*random.nextFloat(), 0, 0);
+			caster.getEntityWorld().spawnParticle(EnumParticleTypes.SMOKE_NORMAL, caster.posX+offX, caster.posY+caster.getEyeHeight()/2.0, caster.posZ+offZ, 0, 0.015*random.nextFloat(), 0, 0);
 		}
 		caster.getEntityData().setInteger(LibMain.LibNBT.inner_fire_tag, 2400);
 	}
--- src/main/java/teamroots/goetia/spellcasting/SpellVoraciousStrike.java
+++ src/main/java/teamroots/goetia/spellcasting/SpellVoraciousStrike.java
@@ -17,13 +17,13 @@ public class SpellVoraciousStrike extends CastSpell{
 		for (float i = 0; i < 360; i += 7.5){
 			float offX = 0.75f*(float)Math.sin(Math.toRadians(i));
 			float offZ = 0.75f*(float)Math.cos(Math.toRadians(i));
-			if (random.nextInt(2) == 0){
-				caster.getEntityWorld().spawnParticle(EnumParticleTypes.CRIT, caster.posX+offX, caster.posY+caster.getEyeHeight()/2.0, caster.posZ+offZ, 0, 0, 0, 0);
+			if (random.nextInt(3) == 0){
+				caster.getEntityWorld().spawnParticle(EnumParticleTypes.FLAME, caster.posX+offX, caster.posY+caster.getEyeHeight()/2.0, caster.posZ+offZ, 0, 0, 0, 0);
 			}
-			if (random.nextInt(2) == 0){
-				caster.getEntityWorld().spawnParticle(EnumParticleTypes.CLOUD, caster.posX+offX, caster.posY+caster.getEyeHeight()/2.0, caster.posZ+offZ, 0, 0.015*random.nextFloat(), 0, 0);
+			if (random.nextInt(3) == 0){
+				caster.getEntityWorld().spawnParticle(EnumParticleTypes.SMOKE_LARGE, caster.posX+offX, caster.posY+caster.getEyeHeight()/2.0, caster.posZ+offZ, 0, 0.015*random.nextFloat(), 0, 0);
 			}
-			caster.getEntityWorld().spawnParticle(EnumParticleTypes.CLOUD, caster.posX+offX, caster.posY+caster.getEyeHeight()/2.0, caster.posZ+offZ, 0, 0.015*random.nextFloat(), 0, 0);
+			caster.getEntityWorld().spawnParticle(EnumParticleTypes.SMOKE_NORMAL, caster.posX+offX, caster.posY+caster.getEyeHeight()/2.0, caster.posZ+offZ, 0, 0.015*random.nextFloat(), 0, 0);
 		}
 		caster.getEntityData().setInteger(LibMain.LibNBT.voracious_strikes_tag, 300);
 	}
