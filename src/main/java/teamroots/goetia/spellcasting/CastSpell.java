e5d5107e95776d8d18ba03fe2cb58fe2b13a54e9
elucent <elucentgames@gmail.com>
2016-08-08 01:29:40 -0400

    Added some buff spell effects.

--- src/main/java/teamroots/goetia/spellcasting/CastSpell.java
+++ src/main/java/teamroots/goetia/spellcasting/CastSpell.java
@@ -2,6 +2,7 @@ package teamroots.goetia.spellcasting;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -11,6 +12,7 @@ import net.minecraft.item.ItemStack;
  */
 public class CastSpell
 {
+	Random random = new Random();
 	public String name;
 	public int impurity;
 	public ItemStack icon;
--- src/main/java/teamroots/goetia/spellcasting/SpellBurningTouch.java
+++ src/main/java/teamroots/goetia/spellcasting/SpellBurningTouch.java
@@ -3,6 +3,7 @@ package teamroots.goetia.spellcasting;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumParticleTypes;
 import teamroots.goetia.lib.LibMain;
 
 public class SpellBurningTouch extends CastSpell{
@@ -13,6 +14,17 @@ public class SpellBurningTouch extends CastSpell{
 	
 	@Override
 	public void doEffect(EntityPlayer caster){
+		for (float i = 0; i < 360; i += 7.5){
+			float offX = 0.75f*(float)Math.sin(Math.toRadians(i));
+			float offZ = 0.75f*(float)Math.cos(Math.toRadians(i));
+			if (random.nextInt(2) == 0){
+				caster.getEntityWorld().spawnParticle(EnumParticleTypes.FLAME, caster.posX+offX, caster.posY+caster.getEyeHeight()/2.0, caster.posZ+offZ, 0, 0, 0, 0);
+			}
+			if (random.nextInt(2) == 0){
+				caster.getEntityWorld().spawnParticle(EnumParticleTypes.SMOKE_LARGE, caster.posX+offX, caster.posY+caster.getEyeHeight()/2.0, caster.posZ+offZ, 0, 0.015*random.nextFloat(), 0, 0);
+			}
+			caster.getEntityWorld().spawnParticle(EnumParticleTypes.SMOKE_NORMAL, caster.posX+offX, caster.posY+caster.getEyeHeight()/2.0, caster.posZ+offZ, 0, 0.015*random.nextFloat(), 0, 0);
+		}
 		caster.getEntityData().setInteger(LibMain.LibNBT.burning_touch_tag, 600);
 	}
 }
