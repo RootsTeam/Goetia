c211731d2602235283e7d51ff8c8e264b78bcda6
Dylan Greer <greerd248@yahoo.com>
2016-08-07 22:41:24 -0400

    Cast spell base

--- src/main/java/teamroots/goetia/spellcasting/CastSpell.java
+++ src/main/java/teamroots/goetia/spellcasting/CastSpell.java
@@ -1,8 +1,23 @@
 package teamroots.goetia.spellcasting;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import net.minecraft.entity.player.EntityPlayer;
+
 /**
  * Created by TeamRoots on 5.8.2016.
  */
 public class CastSpell
 {
+	public int impurity;
+	List<Integer> shape = new ArrayList<Integer>();
+	
+	public CastSpell(int impurity){
+		this.impurity = impurity;
+	}
+	
+	public void doEffect(EntityPlayer caster){
+		
+	}
 }
