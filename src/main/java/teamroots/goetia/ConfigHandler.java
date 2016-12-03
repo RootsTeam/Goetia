da039c6db1e2266fa41d68d47b5cfc1de3894084
AnZaNaMa <recovery22306@gmail.com>
2016-12-02 22:49:28 -0500

    Test

--- src/main/java/teamroots/goetia/ConfigHandler.java
+++ src/main/java/teamroots/goetia/ConfigHandler.java
@@ -31,8 +31,8 @@ public class ConfigHandler
 
     public static void loadConfigs()
     {
-        alignmentBarPosX = config.getInt("alignmentIconOffsetX", Configuration.CATEGORY_GENERAL, 0, -32767, 32767, "The alignment icon's x-offset");
-        alignmentBarPosY = config.getInt("alignmentIconOffsetY", Configuration.CATEGORY_GENERAL, 0, -32767, 32767, "The alignment icon's y-offset");
+        alignmentBarPosX = config.getInt("alignmentIconOffsetX", Configuration.CATEGORY_GENERAL, 0, 0, 32767, "The alignment icon's x-offset");
+        alignmentBarPosY = config.getInt("alignmentIconOffsetY", Configuration.CATEGORY_GENERAL, 0, 0, 32767, "The alignment icon's y-offset");
         alignmentThreshold = config.getInt("alignmentThreshold", Configuration.CATEGORY_GENERAL, 1000, 0, Integer.MAX_VALUE, "How much impurity/purity you must have before becoming a demon/angel");
         alignmentModels = config.getBoolean("alignmentModels", Configuration.CATEGORY_GENERAL, true, "Should models/overlays render on players");
         if (config.hasChanged())
